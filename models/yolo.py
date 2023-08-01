7627aa8cfc6b9d8772deec006133eec33a12ece8
zhaohj <zhaohj@xtad.com>
2023-08-01 13:09:18 +0800

    update

--- models/yolo.py
+++ models/yolo.py
@@ -13,7 +13,7 @@ import platform
 import sys
 from copy import deepcopy
 from pathlib import Path
-from dyhead import DyHeadBlock
+from models.dyhead import DyHeadBlock
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
@@ -363,7 +363,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='yolov5s.yaml', help='model.yaml')
+    parser.add_argument('--cfg', type=str, default='models/yolov5s-dyhead.yaml', help='model.yaml')
     parser.add_argument('--batch-size', type=int, default=1, help='total batch size for all GPUs')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--profile', action='store_true', help='profile model speed')
